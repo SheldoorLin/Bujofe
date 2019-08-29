c5f081977fac941e8b1db09f7ae1eb0fd2ae77be
unknown <lf2netdeep@gmail.com>
2019-08-29 18:38:42 +0800

    test the function of internet pulgin

--- Bujofe/app/src/main/java/com/sheldon/bujofe/calendar/CalendarFragment.kt
+++ Bujofe/app/src/main/java/com/sheldon/bujofe/calendar/CalendarFragment.kt
@@ -1,13 +1,17 @@
 package com.sheldon.bujofe.calendar
 
-import androidx.lifecycle.ViewModelProviders
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.TextView
+import com.roundtableapps.timelinedayviewlibrary.Event
+import com.roundtableapps.timelinedayviewlibrary.EventView
 import com.sheldon.bujofe.R
+import com.sheldon.bujofe.databinding.FragmentCalendarBinding
+import android.util.Log
 
 class CalendarFragment : Fragment() {
 
@@ -17,6 +21,35 @@ class CalendarFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_calendar, container, false)
+
+        val binding = FragmentCalendarBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            val msg = "Selected date is $year/${month + 1}/$dayOfMonth"
+            binding.textView2.text = msg
+        }
+
+
+        val fromValue = System.currentTimeMillis()
+        val toValue = System.currentTimeMillis()
+
+        val myEvent = Event()
+            myEvent.startTime = fromValue.toFloat()
+            myEvent.endTime = toValue.toFloat()
+
+        var myEventView = EventView(this.context,myEvent,
+            setupView = {myView->
+                myView.findViewById<TextView>(R.id.tvTitle).text = "品祥要健身囉"
+            }
+        )
+
+        binding.timeLine.addEvent(myEventView)
+
+
+        val timeStamp = System.currentTimeMillis()
+        Log.d("xxxxx", timeStamp.toString())
+
+
+        return binding.root
     }
 }
