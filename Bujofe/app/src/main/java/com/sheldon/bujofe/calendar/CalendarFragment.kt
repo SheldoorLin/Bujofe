7c628b3748851692f7a261325e9f37d0aa600aa0
unknown <lf2netdeep@gmail.com>
2019-09-02 15:02:53 +0800

    Make code more readable

--- Bujofe/app/src/main/java/com/sheldon/bujofe/calendar/CalendarFragment.kt
+++ Bujofe/app/src/main/java/com/sheldon/bujofe/calendar/CalendarFragment.kt
@@ -6,17 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import com.sheldon.bujofe.R
 import com.sheldon.bujofe.databinding.FragmentCalendarBinding
-import android.util.Log
-import com.sheldon.bujofe.timelayout.Event
-import com.sheldon.bujofe.timelayout.EventView
+import androidx.lifecycle.ViewModelProviders
 
 
 class CalendarFragment : Fragment() {
 
-    private lateinit var viewModel: CalendarViewModel
+    private val viewModel: CalendarViewModel by lazy {
+        ViewModelProviders.of(this).get(CalendarViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,78 +23,22 @@ class CalendarFragment : Fragment() {
 
         val binding = FragmentCalendarBinding.inflate(inflater, container, false)
         binding.lifecycleOwner = this
+
+
         binding.calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
             val msg = "Selected date is $year/${month + 1}/$dayOfMonth"
             binding.textView2.text = msg
         }
 
-
-        val myEventView = EventView(
-            context,
-            Event().apply {
-                startTime = 1F
-                endTime = 3F
-                title = "1"
-//              Log.d("AAmyEvent1","${myEvent1.toString()}")
-            },
-            itemsMargin = 1, //optional
-            layoutResourceId = R.layout.item_event_three, //optional
-            setupView = { myView ->
-                //SETUP VIEW
-                myView.findViewById<TextView>(R.id.tvTitle).text = "品翔要健身囉"
-                Log.d("Sheldon", "setupView1,I'am here")
-            },
-            onItemClick = { event ->
-                //CLICK EVENT
-                Log.d("Sheldon", "I'am here,onItemClick")
-            }
-        )
-        binding.timeLine.addEvent(myEventView)
-
-        val myEventView2 = EventView(context,
-            Event().apply {
-                startTime = 1F
-                endTime = 6F
-                title = "2"
-
-            },//加入開始結束時間
-            itemsMargin = 1, //與旁邊方塊的間隔
-            layoutResourceId = R.layout.item_event, //使用甚麼顏色的背景
-            setupView = { myView ->
-                //SETUP VIEW
-                myView.findViewById<TextView>(R.id.tvTitle).text = "我要健身囉"
-                Log.d("Sheldon", "setupView2,I'am here")
-            },
-            onItemClick = { event ->
-                //CLICK EVENT
-                Log.d("Sheldon", "I'am here,onItemClick")
-            }
-        )
-        binding.timeLine.addEvent(myEventView2)
+        
 
 
-        val myEventView3 = EventView(context,
-            Event().apply {
-                startTime = 2F
-                endTime = 4F
-                title = "2"
-
-            },
-            itemsMargin = 1, //optional
-            layoutResourceId = R.layout.item_event, //optional
-            setupView = { myView ->
-                //SETUP VIEW
-                myView.findViewById<TextView>(R.id.tvTitle).text = "吃飯時間"
-                Log.d("Sheldon", "setupView2,I'am here")
-            },
-            onItemClick = { event ->
-                //CLICK EVENT
-                Log.d("Sheldon", "I'am here,onItemClick")
-            }
-        )
-        binding.timeLine.addEvent(myEventView3)
-
-
+        //加入事件
+        binding.timeLine.run {
+            addEvent(viewModel.myEventView)
+            addEvent(viewModel.myEventView2)
+            addEvent(viewModel.myEventView3)
+        }
 
 
         return binding.root
--- Bujofe/app/src/main/java/com/sheldon/bujofe/calendar/CalendarViewModel.kt
+++ Bujofe/app/src/main/java/com/sheldon/bujofe/calendar/CalendarViewModel.kt
@@ -1,7 +1,80 @@
 package com.sheldon.bujofe.calendar
 
+import android.util.Log
+import android.widget.TextView
 import androidx.lifecycle.ViewModel;
+import com.sheldon.bujofe.BujofeApplication
+import com.sheldon.bujofe.R
+import com.sheldon.bujofe.timelayout.Event
+import com.sheldon.bujofe.timelayout.EventView
 
 class CalendarViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+
+
+    val myEventView = EventView(
+        BujofeApplication.instance.applicationContext,
+        Event().apply {
+            startTime = 1F
+            endTime = 3F
+            title = "1"
+//              Log.d("AAmyEvent1","${myEvent1.toString()}")
+        },//加入開始結束時間
+        itemsMargin = 1, //與旁邊方塊的間隔(可加可不加)
+        layoutResourceId = R.layout.item_event_three,//使用甚麼顏色的背景(不加就默認第一種)
+        setupView = { myView ->
+            //SETUP VIEW
+            myView.findViewById<TextView>(R.id.tvTitle).text = "品翔要健身囉"
+            Log.d("Sheldon", "setupView1,I'am here")
+        },
+        onItemClick = { event ->
+            //CLICK EVENT
+            Log.d("Sheldon", "I'am here,onItemClick")
+        }
+    )
+
+    val myEventView2 = EventView(
+        BujofeApplication.instance.applicationContext,
+        Event().apply {
+            startTime = 1F
+            endTime = 6F
+            title = "2"
+
+        },//加入開始結束時間
+        itemsMargin = 1, //與旁邊方塊的間隔(可加可不加)
+        layoutResourceId = R.layout.item_event, //使用甚麼顏色的背景(不加就默認第一種)
+        setupView = { myView ->
+            //SETUP VIEW
+            myView.findViewById<TextView>(R.id.tvTitle).text = "我要健身囉"
+            Log.d("Sheldon", "setupView2,I'am here")
+        },
+        onItemClick = { event ->
+            //CLICK EVENT
+            Log.d("Sheldon", "I'am here,onItemClick")
+        }
+    )
+
+    val myEventView3 = EventView(
+        BujofeApplication.instance.applicationContext,
+        Event().apply {
+            startTime = 2F
+            endTime = 4F
+            title = "2"
+
+        },//加入開始結束時間
+        itemsMargin = 1,//與旁邊方塊的間隔(可加可不加)
+        layoutResourceId = R.layout.item_event, //使用甚麼顏色的背景(不加就默認第一種)
+        setupView = { myView ->
+            //SETUP VIEW
+            myView.findViewById<TextView>(R.id.tvTitle).text = "吃飯時間"
+            Log.d("Sheldon", "setupView2,I'am here")
+        },
+        onItemClick = { event ->
+            //CLICK EVENT
+            Log.d("Sheldon", "I'am here,onItemClick")
+        }
+    )
+
+
+
 }
